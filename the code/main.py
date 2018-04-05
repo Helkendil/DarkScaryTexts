98579144ea04448cc4fdfffe7fd5c0feb498ec2b
KolosovMax <kolosovmax@kometeh.ru>
2018-04-05 12:07:39 +0300

    First commit. I hope not last

---
+++ the code/main.py
@@ -0,0 +1,6 @@
+__author__ = 'maxim'
+print("#######################################################\r\n")
+print("For python files we'll using pyCharm Community Edition")
+print("By the way, we need to create our Virtual Environment, it is only available in pyCharm new versions\r\n")
+print("#######################################################")
+pass
